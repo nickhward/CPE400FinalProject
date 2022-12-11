34f93b0c8d2e6a32e51567aab2d97a4e76b02f53
Compton-Kiara <kdcmpton23@nevada.unr.edu>
2022-12-11 15:30:55 -0800

    Added implementation of removing node with the greatest failure

--- graph.py
+++ graph.py
@@ -20,13 +20,7 @@ def mainMenu(graph, failures):
 
             #continue
         if user == "1":
-            #call the simulation
-            pos=nx.spring_layout(graph)
-            nx.draw_networkx(graph,pos)
-            labels = nx.get_edge_attributes(graph,'weight')
-            nx.draw_networkx_edge_labels(graph,pos,edge_labels=labels)
-            plt.savefig("simulated_graph.png")
-                        
+            #call the simulation           
             print("User chose 1")
             simulation(graph, failures)
         elif user == "2":
@@ -49,7 +43,7 @@ def simulation(graph, failures):
     print("Inside of simulation")
     randomFailures = []
     i =0
-    #Randomly Generating failures for each node
+    #Randomly Generating failures between 0 and 1 for each node
     #------------------------------------------
     while i < len(failures):
         fail = random.uniform(0,1)
@@ -58,21 +52,66 @@ def simulation(graph, failures):
         i += 1
     print("Random Failures array: ", randomFailures)
 
-def simulate_failure(probability):
+    #for each node in the passed in failures and randomly generated failures, check if the value is higher in the random failures
+    #if so keep the index of the node and if there's multiple, check which value has the greatest; if equal delete the one with highest weighted edge
+    #-------------------------------------------------------------------------------------------------------------------------------------------------
+    deleteIndex = []
+    for x, y in zip(failures, randomFailures):
+        #x = element in failures
+        #y = element in randomFailures
+        if y > x:
+            deleteIndex.append(randomFailures.index(y))
+            print("The element in failures: ", x)
+            print("The element in random failures: ", y)
+
+    print("List of indices of elements to delete: ", deleteIndex)
+    #check if the size of the list of possible to be deleted nodes greater than 1, if so check which one has the largest difference
+    differences = []
+    if len(deleteIndex) > 1:
+        for e in deleteIndex:
+            diff = randomFailures[e] - failures[e]
+            differences.append(diff)
+    
+        print("Differences: ", differences)
+        #finding largest difference and storing its index to use to get node supposed to delete from graph
+        idx = differences.index(max(differences))
+        print("Want index of the largest difference: ", idx)
+
+    else:
+        #means there was only one node in the graph who's random failure was greater than their initital
+        #then the element you want to delete is the index provided in the deleteIndex list
+        idx = deleteIndex[0]
+    
+    #getting list of nodes to retrieve the node at the founded index that had the largest failure difference
+    nodeList = list(graph.nodes)
+    node = nodeList[idx]
+    print("Node want to delete: ", node)
+    #removing node
+    graph.remove_node(node)
+
+    #printing new graph with removed node
+    #***ADD HERE HOW TO RECONNECT GRAPH WITH DIJKSTRA and will print the graph with node removed and it being reconnected***
+    pos=nx.spring_layout(graph)
+    nx.draw_networkx(graph,pos)
+    labels = nx.get_edge_attributes(graph,'weight')
+    nx.draw_networkx_edge_labels(graph,pos,edge_labels=labels)
+    plt.savefig("simulated_graph.png")
+
+#def simulate_failure(probability):
     """_summary_
 
     Args:
         probability (_type_): _description_
     """
      # Generate a random number between 0 and 1
-    rand_num = np.random.random()
+    #rand_num = np.random.random()
     
     # If the random number is less than the given probability,
     # the node/link is considered failed
-    if rand_num < probability:
-        return True
-    else:
-        return False
+    #if rand_num < probability:
+        #return True
+    #else:
+        #return False
 
 
 def main():
